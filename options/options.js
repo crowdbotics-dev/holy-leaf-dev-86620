c3d9576d781c2f068866cbce101fd589b731a46c
Crowdbotics <engineering@crowdbotics.com>
2023-05-28 00:44:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holy-leaf-dev-86620",
+  url: "https://holy-leaf-dev-86620.botics.co",
+  api: "https://holy-leaf-dev-86620.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
